c0ecdfe13c2a195144a8720d87d987ac854e0728
Stiven Quiñonez <stivengonzalez0100@gmail.com>
2020-02-08 13:27:27 -0600

    tarea 2.0

---
+++ meses.js
@@ -0,0 +1,24 @@
+var nombre = "estaciones del año<br/>";
+console.log(nombre);
+document.write(nombre);
+
+var mes = 10;
+if(mes==12 || mes==1 || mes==2) {
+    console.log("es invierno");
+    document.write("es invierno");
+} else if (mes >= 3 && mes <= 5) {
+    console.log("es primavera");
+    document.write("es primavera");
+} else if (mes >= 6 && mes <= 8) {
+    console.log("es verano");
+    document.write("es verano");
+} else if (mes >= 9 && mes <= 11) {
+    console.log("es otoño");
+    document.write("es otoño");
+} else {
+    console.log("mes ingresado no existe");
+    document.write("mes ingresado no existe");
+}
+var nombre = " <br/><br/><br/>Stiven Giovanny Quiñonez Gonzalez<br/>20004894"
+document.write(nombre);
+
